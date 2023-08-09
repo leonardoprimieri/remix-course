6034a8fc3cc6f71cbc410f3e4b6ac574b7dafe91
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-08-09 09:48:05 -0300

    ✨ initial pages and components

---
+++ app/routes/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "@remix-run/react";
+
+import styles from "../styles/home.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+
+export default function Index() {
+  return (
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our early beta and never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try now!</Link>
+      </p>
+    </main>
+  );
+}
---
+++ remix.config.js
@@ -0,0 +1,17 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  future: {
+    v2_dev: false,
+    v2_errorBoundary: false,
+    v2_headers: false,
+    v2_meta: false,
+    v2_normalizeFormMethod: false,
+    v2_routeConvention: false,
+  },
+};
